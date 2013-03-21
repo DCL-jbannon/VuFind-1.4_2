6d2a3caf5aa1856f6dc5724911f41e4f74f364ee
jgimenez <jgimenez@28506cad-9c98-f24e-950d-e205d8e2a2e8>
2013-03-21 22:03:13 +0000

    Deleted Test configurations no needed on Prod environtment.
    git-svn-id: svn://vufind-dev/VuFind-PlusProd/trunk@98 28506cad-9c98-f24e-950d-e205d8e2a2e8

--- sites/vufindtest.douglascountylibraries.org/vufindtest.douglascountylibraries.org.bat
+++
@@ -1,6 +0,0 @@
-@set VUFIND_HOME=E:\projects\VuFind-Plus\sites\vufindtest.douglascountylibraries.org
-@set JETTY_HOME=E:\projects\VuFind-Plus\sites\vufindtest.douglascountylibraries.org\solr\jetty
-@set SOLR_HOME=E:\projects\VuFind-Plus\sites\vufindtest.douglascountylibraries.org\solr
-@set JETTY_PORT=8080
-@set JETTY_LOG=E:\projects\VuFind-Plus\sites\vufindtest.douglascountylibraries.org\logs\jetty
-@call run_vufind.bat start %1 %2 %3 %4 %5 %6 %7 %8 %9
