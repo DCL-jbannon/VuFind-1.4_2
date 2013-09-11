e9d37b19df0b9e6a59fa7d89dee05db20120e111
jgimenez <jgimenez@28506cad-9c98-f24e-950d-e205d8e2a2e8>
2013-09-11 15:43:14 +0000

    Added to enable us stop solrdbslave01/stopsolr.bat
    git-svn-id: svn://vufind-dev/VuFind-PlusProd/trunk@242 28506cad-9c98-f24e-950d-e205d8e2a2e8

---
+++ sites/solrdbslave01/stopsolr.bat
@@ -0,0 +1,6 @@
+@set VUFIND_HOME=E:\projects\VuFind-Plus\sites\solrdbslave01
+@set JETTY_HOME=E:\projects\VuFind-Plus\sites\solrdbslave01\solr\jetty
+@set SOLR_HOME=E:\projects\VuFind-Plus\sites\solrdbslave01\solr
+@set JETTY_PORT=8080
+@set JETTY_LOG=E:\projects\VuFind-Plus\sites\solrdbslave01\logs\jetty
+@call run_vufind.bat stop %1 %2 %3 %4 %5 %6 %7 %8 %9
